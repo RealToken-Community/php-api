a3dd7258e9d4853a1694dbefadcabe540d738b2a
Sigri44 <sigri44@hotmail.fr>
2020-10-03 18:20:50 +0200

    Update test env

--- src/Controller/TokenController.php
+++ src/Controller/TokenController.php
@@ -14,7 +14,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Role\Role;
 
 /**
  * @Route("/v1")
