a2cdc42a5dad2076871db3d44f43cb9173444e42
Sigri44 <sigri44@hotmail.fr>
2020-11-01 18:06:00 +0100

    Add rent per day

---
+++ migrations/Version20201012183744.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201012183744 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE tokens ADD last_update DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP');
+        $this->addSql('ALTER TABLE tokens ADD net_rent_day DOUBLE PRECISION DEFAULT NULL');
+        $this->addSql('ALTER TABLE tokens ADD net_rent_day_per_token DOUBLE PRECISION DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE tokens DROP last_update');
+        $this->addSql('ALTER TABLE tokens DROP net_rent_day');
+        $this->addSql('ALTER TABLE tokens DROP net_rent_day_per_token');
+    }
+}
