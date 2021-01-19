8ada9120af3f90cd80a89009b1592d5b0de5745e
Sigri44 <sigri44@hotmail.fr>
2021-01-19 15:42:11 +0100

    Update CircleCI

--- config/deploy/symfony/realtapi.rb
+++ config/deploy/symfony/realtapi.rb
@@ -7,9 +7,9 @@ namespace :docker do
         on roles(:staging) do
             within release_path do
                 # Restart containers on Staging
-                execute "sudo", "docker-compose", "build"
-                execute "sudo", "docker-compose", "-f", "docker-compose.preprod.yml", "up", "-d", "--force"
-                execute "sudo", "docker-compose", "-f", "docker-compose.preprod.yml", "exec", "-T", "symfony-preprod", "php", "bin/console", "doctrine:migrations:migrate"
+                execute "docker-compose", "build"
+                execute "docker-compose", "-f", "docker-compose.preprod.yml", "up", "-d", "--force"
+                execute "docker-compose", "-f", "docker-compose.preprod.yml", "exec", "-T", "symfony-preprod", "php", "bin/console", "doctrine:migrations:migrate"
 
             end
         end
@@ -17,9 +17,9 @@ namespace :docker do
         on roles(:production) do
             within release_path do
                 # Restart containers on Production
-                execute "sudo", "docker-compose", "build"
-                execute "sudo", "docker-compose", "-f", "docker-compose.yml", "up", "-d", "--force"
-                execute "sudo", "docker-compose", "-f", "docker-compose.yml", "exec", "-T", "symfony", "php", "bin/console", "doctrine:migrations:migrate"
+                execute "docker-compose", "build"
+                execute "docker-compose", "-f", "docker-compose.yml", "up", "-d", "--force"
+                execute "docker-compose", "-f", "docker-compose.yml", "exec", "-T", "symfony", "php", "bin/console", "doctrine:migrations:migrate"
             end
         end
     end
