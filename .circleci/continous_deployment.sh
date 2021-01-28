d367322a1e273381f8c3cf86625e41e359775804
Sigri44 <sigri44@hotmail.fr>
2021-01-28 20:23:56 +0100

    Update CircleCI

--- .circleci/continous_deployment.sh
+++ .circleci/continous_deployment.sh
@@ -3,15 +3,15 @@
 WORKING_DIR=${PWD}
 
 # Remove linked .env
-rm .env*
+rm current/.env*
 # Copy real .env files
-cp "${WORKING_DIR}"/shared/.env* .
+cp "${WORKING_DIR}"/shared/.env* current/
 
 # Docker build
-sudo docker build .
+docker build .
 
 # Docker run
-sudo docker-compose -f docker-compose.preprod.yml up -d --force
+docker-compose -f docker-compose.preprod.yml up -d --force
 
 # Migrate
-sudo docker-compose -f docker-compose.preprod.yml exec -T symfony-preprod php bin/console doctrine:migrations:migrate
+docker-compose -f docker-compose.preprod.yml exec -T symfony-preprod php bin/console doctrine:migrations:migrate
