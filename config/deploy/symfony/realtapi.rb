28029f8b582e7953a3b0078b6ddb0ebaf7d55df2
Sigri44 <sigri44@hotmail.fr>
2021-01-28 10:38:37 +0100

    Update CircleCI

--- config/deploy/symfony/realtapi.rb
+++ config/deploy/symfony/realtapi.rb
@@ -7,7 +7,7 @@ namespace :docker do
         on roles(:staging) do
             within release_path do
                 # Restart containers on Staging
-                execute "./config/continous_deployment.sh"
+                execute "./.circleci/continous_deployment.sh"
 #                 execute "pwd"
 #                 execute "ls", "-lah"
 #                 execute "docker-compose", "build"
