4912807486afdd2a8dcb86cc60712c2fea8cfd58
Sigri Denver <sigri44@hotmail.fr>
2021-04-27 01:04:55 +0200

    Prepare tokenList

--- src/Service/DefiService.php
+++ src/Service/DefiService.php
@@ -22,6 +22,9 @@ class DefiService extends Service
     {
         $ammList = $this->getCommunityList($refer);
 
+        // TODO : future list
+//        $ammList = $this->tokenListMapping();
+
         return new JsonResponse($ammList, Response::HTTP_OK);
     }
 
@@ -52,5 +55,101 @@ class DefiService extends Service
         curl_close($curl);
 
         return json_decode($response, true);
+    }
+
+    /**
+     * Token list mapping.
+     *
+     * @return array
+     */
+    private function tokenListMapping()
+    {
+        return [
+            "name"      =>	"RealToken",
+            "logoURI"   =>	"https://realt.co/wp-content/uploads/2019/01/cropped-RealToken_Favicon.png",
+            "timestamp" =>	new \DateTime(),
+            "version"   => [
+                "major" => 2,
+                "minor" => 0,
+                "patch" => 0
+            ],
+            "keywords" => [
+                0 => "Uniswap",
+                1 => "DeFi",
+                2 => "RealT",
+                3 => "Ethereum",
+                4 => "xDai chain",
+            ],
+            "tags" => [
+                "ETH" => [
+                    "name"          => "Ethereum",
+                    "description"   => "RealToken on Ethereum mainnet, chainID 1"
+                ],
+                "MATIC" => [
+                    "name"          => "Matic network",
+                    "description"   => "RealToken on Matic mainnet, chainID 137"
+                ],
+                "mumbai" => [
+                    "name"          => "Mumbai Testnet",
+                    "description"   => "RealToken on Mumbai Testnet, chainID 80001"
+                ],
+                "xDai" => [
+                    "name"          => "xDai chain",
+                    "description"   => "RealToken on xDai mainnet, chainID 100"
+                ],
+                "sokol" => [
+                    "name"          => "Sokol Testnet",
+                    "description"   => "RealToken on Sokol Testnet, chainID ....."
+                ],
+                "gorli" => [
+                    "name"          => "Goerli TestNet",
+                    "description"   => "RealToken on Goerli Testnet, chainID 5"
+                ],
+                "stablecoin" => [
+                    "name"          => "Stable coin",
+                    "description"   => "Stablecoin, crypto with a stable value often equivalent to US Dollars"
+                ],
+                "aave" => [
+                    "name"          => "Stable coin Aave",
+                    "description"   => "Stablecoin Aave, crypto with a stable value often equivalent to US dollars and which generates interest via the Aave platform"
+                ],
+                "uniV1" => [
+                    "name"          => "Uniswap V1",
+                    "description"   => "Available on Uniswap V1"
+                ],
+                "uniV2" => [
+                    "name"          => "Uniswap V2",
+                    "description"   => "Available on Uniswap V2"
+                ],
+                "honey" => [
+                    "name"          => "HoneySwap",
+                    "description"   => "Available on HoneySwap"
+                ],
+                "levin" => [
+                    "name"          => "LevinSwap",
+                    "description"   => "Available on LevinSwap"
+                ],
+                "pairLevin" => [
+                    "name"          => "LEVIN pair",
+                    "description"   => "The available peer for this pool is RealToken and LEVIN"
+                ],
+                "SF" => [
+                    "name"          => "Single family",
+                    "description"   => "RealT Single family property"
+                ],
+                "MF" => [
+                    "name"          => "Multi family",
+                    "description"   => "RealT Multi family property"
+                ],
+                "2020" => [
+                    "name"          => "Sold in 2020",
+                    "description"   => "House tokenizer and put up for sale by RealT in 2020"
+                ],
+                "2021" => [
+                    "name"          => "Sold in 2021",
+                    "description"   => "House tokenizer and put up for sale by RealT in 2021"
+                ]
+            ],
+        ];
     }
 }
