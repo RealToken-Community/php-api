3f0604f7f2e0f3aa5cf6322b2d9efe9514eaf43b
Sigri44 <sigri44@hotmail.fr>
2021-09-28 22:18:58 +0200

    Fix new syntax

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -26,17 +26,17 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
      * Used to upgrade (rehash) the user's password automatically over time.
      *
      * @param UserInterface $user
-     * @param string $newEncodedPassword
+     * @param string $newHashedPassword
      * @throws ORMException
      * @throws OptimisticLockException
      */
-    public function upgradePassword(UserInterface $user, string $newEncodedPassword): void
+    public function upgradePassword(UserInterface $user, string $newHashedPassword): void
     {
         if (!$user instanceof User) {
             throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', \get_class($user)));
         }
 
-        $user->setPassword($newEncodedPassword);
+        $user->setPassword($newHashedPassword);
         $this->_em->persist($user);
         $this->_em->flush();
     }
