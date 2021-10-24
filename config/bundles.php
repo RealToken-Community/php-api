191c3e7cb2a06528947f183f1c66e4d7ac2fe50a
Gaultier Boniface <gboniface@enoptea.fr>
2021-10-24 12:29:48 +0200

    Install Sentry bundle in Symfony production mode.

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['prod' => true],
 ];
