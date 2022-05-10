35bc5d9baa97d738bd13b4624518ce1acde90a0f
Sigri44 <sigri44@hotmail.fr>
2022-05-11 00:49:06 +0200

    Fix refer condition

--- src/Service/AuthenticatorService.php
+++ src/Service/AuthenticatorService.php
@@ -36,7 +36,7 @@ class AuthenticatorService extends Service
         }
 
         // Match application with refer
-        if (!empty($refer) && !empty($application->getRefer()) && $refer !== $application->getRefer()) {
+        if (!empty($application->getRefer()) && $refer !== $application->getRefer()) {
             throw new HttpException(Response::HTTP_UNAUTHORIZED, 'Invalid refer source : ' . $refer);
         }
     }
@@ -127,7 +127,7 @@ class AuthenticatorService extends Service
             }
 
             // Match application with refer
-            if (!empty($refer) && !empty($application->getRefer()) && $refer !== $application->getRefer()) {
+            if (!empty($application->getRefer()) && $refer !== $application->getRefer()) {
                 throw new HttpException(Response::HTTP_UNAUTHORIZED, 'Invalid refer source : ' . $refer);
             }
 
