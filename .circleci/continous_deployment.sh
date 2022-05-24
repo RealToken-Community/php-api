6e10553975de7135c6ef733f83f8d65c9f7becd0
Sigri44 <sigri44@hotmail.fr>
2022-05-25 00:05:23 +0200

    Optimize Docker deployment

--- .circleci/continous_deployment.sh
+++ .circleci/continous_deployment.sh
@@ -11,7 +11,7 @@ cp "${WORKING_DIR}"/shared/.env* .
 docker build .
 
 # Docker run
-docker-compose -f docker-compose.preprod.yml up -d --force
+docker-compose -f docker-compose.preprod.yml up -d --force symfony-preprod
 
 # Migrate
 docker-compose -f docker-compose.preprod.yml exec -T symfony-preprod php bin/console doctrine:migrations:migrate -q
