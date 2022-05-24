0087f979a1ebf8a6a58b8859514f6432b7cb0eac
Sigri44 <sigri44@hotmail.fr>
2022-05-24 23:43:45 +0200

    Update Capistrano config

--- config/deploy/symfony/realtapi/staging.rb
+++ config/deploy/symfony/realtapi/staging.rb
@@ -1,6 +1,6 @@
 set :stage, :staging
 
-server "preprod.realt.community", user: "root", port: 2222, roles: %w{all staging}
+server "preprod.realt.community", user: "realt", port: 2222, roles: %w{all staging}
 
 # The user that the server is running under (used for ACLs)
 # set :symfony_server_user, 'realt'
